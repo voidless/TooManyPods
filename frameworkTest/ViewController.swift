c526942b2febfb1ce45c5d1f5e96432ea9dc25c0
Ilya Bersenev <mail@voidless.ru>
2021-03-15 19:38:00 +0300

    Initial commit

---
+++ frameworkTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  frameworkTest
+//
+//  Created by Ilya Bersenev on 15.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
