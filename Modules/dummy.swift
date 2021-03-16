fd62487a7f9e5087587bf253bca4201421bc74e8
Ilya Bersenev <mail@voidless.ru>
2021-03-16 13:23:30 +0300

    add public code to dummy.swift

--- Modules/dummy.swift
+++ Modules/dummy.swift
@@ -1,7 +1,21 @@
 import Foundation
 
-class ModuleDependencies {
+struct ModuleDependencies {
+    var external: ModuleExternalDependencies?
+
     var name: String = "!"
 }
 
 var Dependencies = ModuleDependencies()
+
+public struct ModuleExternalDependencies {
+    public var some: Int
+
+    public init(some: Int) {
+        self.some = some
+    }
+}
+
+public func SetupModule(_ external: ModuleExternalDependencies) {
+    Dependencies.external = external
+}
